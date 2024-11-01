b8208e21686f37da24ab2e022ef4c93d2988be78
John Lancaster <johnlancaster@Johns-MacBook-Air.local>
2024-11-01 13:17:39 -0400

    Updated run.py to remove inferred schema and simplify code

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -90,11 +90,11 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
-    signature = mlflow.models.infer_signature(X_val, y_pred)
+    #signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
         sk_pipe,
         path="random_forest_dir",
-        signature=signature,
+        #signature=signature,
         input_example=X_train.iloc[:5]
     )
     ######################################
